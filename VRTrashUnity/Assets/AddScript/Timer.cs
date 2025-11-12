89391cb84c16e62cf051005b227b9ff8ef0f858b
JamterFondy <jedwind777@gmail.com>
2025-11-12 18:33:52 +0900

    細かい修正（カメラに追従する）Canvasの子オブジェクトにTimerを設定

--- VRTrashUnity/Assets/AddScript/Timer.cs
+++ VRTrashUnity/Assets/AddScript/Timer.cs
@@ -4,6 +4,8 @@ public class Timer : MonoBehaviour
 {
     [SerializeField] private NumberBank numberBank;
     private float time;
+    private float logTimer = 0f; // ログ用のタイマー
+    private const float LOG_INTERVAL = 1f; // 1秒ごとにログ出す
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
@@ -22,11 +24,17 @@ public class Timer : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        Debug.Log($"残り{(int)time}秒");
 
         if (time > 0)
         {
             time -= Time.deltaTime; // テスト減算
+        }
+
+        logTimer += Time.deltaTime;
+        if (logTimer >= LOG_INTERVAL)
+        {
+            Debug.Log($"残り{(int)time}秒");
+            logTimer = 0f;
         }
     }
 
