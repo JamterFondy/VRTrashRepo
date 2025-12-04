db3e07ccdcbee632d20f6e2100608011bca9d291
JamterFondy <94736291+JamterFondy@users.noreply.github.com>
2025-12-04 20:02:45 +0900

    ４度目のビルド＆スタートボタン微調整

--- VRTrashUnity/Assets/AddScript/StartButton.cs
+++ VRTrashUnity/Assets/AddScript/StartButton.cs
@@ -8,12 +8,13 @@ public class StartButton : MonoBehaviour
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        //var button = GetComponent<Button>();
+        var button = GetComponent<Button>();
 
-       // button.onClick.AddListener(() =>
-       // {
-            
-       // });
+       button.onClick.AddListener(() =>
+       {
+           SceneManager.LoadScene("BaseScene");
+           SceneManager.LoadScene("SampleScene", LoadSceneMode.Additive);
+       });
     }
 
     // Update is called once per frame
